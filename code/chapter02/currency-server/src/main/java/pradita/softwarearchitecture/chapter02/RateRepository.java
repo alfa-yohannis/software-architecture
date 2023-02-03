e38c3ea6538c73d78bc59afee7723b97cad10cdb
Alfa Yohannis <ary506@york.ac.uk>
2023-02-03 16:19:17 +0000

    Update RateRepository.java

--- code/chapter02/currency-server/src/main/java/pradita/softwarearchitecture/chapter02/RateRepository.java
+++ code/chapter02/currency-server/src/main/java/pradita/softwarearchitecture/chapter02/RateRepository.java
@@ -2,7 +2,7 @@ package pradita.softwarearchitecture.chapter02;
 
 import java.util.Collection;
 
-import org.springframework.data.jpa.repository.Query;
+//import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 
@@ -10,7 +10,7 @@ import org.springframework.data.repository.CrudRepository;
 public interface RateRepository extends CrudRepository<Rate, Integer> {
   
   //JPQL
-  @Query("SELECT r FROM Rate r WHERE r.fromCurrency = ?1 and r.toCurrency = ?2")
+  //@Query("SELECT r FROM Rate r WHERE r.fromCurrency = ?1 and r.toCurrency = ?2")
   Collection<Rate> findFirstByFromCurrencyAndToCurrency(String fromCurrency, String toCurrency);
   
 }
